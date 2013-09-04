28ddb4f03510a57dc5468fae1a894b7f2540c2c1
Sutee Dee <thaiyoshi@Sutees-MacBook-Pro.local>
2013-09-04 01:46:28 -0700

    support fraction

--- unit.js
+++ unit.js
@@ -4,30 +4,52 @@ var Converter = function() {
 	console.log("init Converter");
 	this.units = [
     {"name": "cup", "numBaseUnits": 48.0},
-    {"name": "tablespoon", "numBaseUnits": 3.0}
+    {"name": "tablespoon", "numBaseUnits": 3.0},
+    {"name": "teaspoon", "numBaseUnits": 1.0}
 	].sort(unitSizeCompare); //rev sorted by size
 	this.prettyDisplay = prettyDisplay;	
 
 	function prettyDisplay(quantity){
+		var MIN_FRAC = 0.125;
 		var components = quantity.split(" ");
 		var numUnits = parseFloat(components[0]);
 		var unitName = components[1];
 
 		for (i= 0; i < this.units.length; ++i){ //start with largest unit and work down to smallest
 			currUnit = this.units[i];
-			if (numUnits >=  currUnit.numBaseUnits ){
+			if (numUnits >=  currUnit.numBaseUnits * MIN_FRAC) {
 				console.log("Selected "+currUnit.name);
 				prettyQuantity=numUnits/currUnit.numBaseUnits;
+
+				if (prettyQuantity % 1 !=0) {
+					console.log("fractional "+numUnits+"/"+currUnit.numBaseUnits);
+					rounded = roundToNearestFrac(prettyQuantity, MIN_FRAC);
+					console.log("Rounded "+rounded);
+					f = new Fraction(rounded);
+					prettyQuantity = f.numerator + '/' + f.denominator;
+				}
 				prettyUnit = currUnit.name;
+				return [prettyQuantity,prettyUnit].join(" ");
 			}
-			return [prettyQuantity,prettyUnit].join(" ");
 		}
+	}
+}
+
+/*
+utility functions
+*/
+var roundToNearestFrac = function (num,roundBy) {
+	f = new Fraction(roundBy);
+	if (f.numerator===1) {
+		return Math.floor(num*f.denominator)/f.denominator;
+	}
+	else {
+		return null;
 	}
 
 }
 
-
-var unitSizeCompare  =function (a,b) {
+var unitSizeCompare  = function (a,b) {
   if (a.quantity > b.quantity)
      return -1;
   if (a.quantity < b.quantity)
